2d11482eeff451a7283c23bb5997480d968353c2
dcruzcruz <dcruzcruz@academic.rrc.ca>
2024-03-24 19:03:16 -0500

    Home Index view added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.page(params[:page]).per(6)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Delete existing AdminUser in development
+AdminUser.where(email: 'admin@example.com').destroy_all if Rails.env.development?
+
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+5.times do
+  Category.create(name: Faker::Creature::Animal.unique.name)
+end
+
+# Seed products
+11.times do
+  category = Category.order("RANDOM()").first
+  product = Product.create(
+    product_name: Faker::Commerce.product_name,
+    description: Faker::Lorem.paragraph(sentence_count: 2),
+    price: Faker::Commerce.price(range: 10.0..100.0),
+    stock: Faker::Number.between(from: 0, to: 100),
+    category_id: category.id
+  )
+end
