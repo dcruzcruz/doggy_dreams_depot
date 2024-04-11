48aa6894565e42d0f404bbb5348d2755b0738ce5
dcruzcruz <dcruzcruz@academic.rrc.ca>
2024-04-10 23:38:22 -0500

    Province and Address added to Active Admin and Users model

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,8 +1,12 @@
 class Province < ApplicationRecord
 
   def self.ransackable_attributes(auth_object = nil)
-    ["created_at", "gst_rate", "hst_rate", "id", "name", "pst_rate", "updated_at"]
+    ["users_id_eq", "created_at", "gst_rate", "hst_rate", "id", "name", "pst_rate", "updated_at"]
   end
 
-  has_many :customers
+  def self.ransackable_associations(auth_object = nil)
+    ["users"]
+  end
+
+  has_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,27 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  belongs_to :province
+
+  # Add the address attribute
+  attribute :address, :string
+  attribute :province
 
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "email", "encrypted_password", "id", "remember_created_at", "reset_password_sent_at", "reset_password_token", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[email address province_id] # Add 'address' if you want to allow searching for it
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[province_id_lt province_id_gt province_id_eq email reset_password_token address_cont address_eq address_start address_end] # Add 'reset_password_token' if you want to allow searching for it
   end
 
   # Include default devise modules. Others available are:
